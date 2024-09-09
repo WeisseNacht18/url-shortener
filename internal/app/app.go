5b1390f08ef1c8d6b064ff0e4f3f2e7e34e20963
WhiteNight18 <white18night@yandex.ru>
2024-09-09 16:20:41 +0300

    added unit tests

---
+++ internal/app/app.go
@@ -0,0 +1,56 @@
+package app
+
+import (
+	"io"
+	"net/http"
+	"strings"
+
+	"github.com/google/uuid"
+)
+
+var (
+	shortUrls map[string]string
+)
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost && strings.Contains(r.Header.Get("Content-Type"), "text/plain") {
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		link := string(body)
+		id := uuid.New()
+		shortLink := id.String()[:8]
+		shortUrls[shortLink] = link
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte("http://" + r.Host + "/" + shortLink))
+		return
+	} else if r.Method == http.MethodGet {
+		id := r.URL.Path[1:]
+		value, ok := shortUrls[id]
+		if ok {
+			w.Header().Set("Location", value)
+			w.WriteHeader(http.StatusTemporaryRedirect)
+		} else {
+			w.WriteHeader(http.StatusBadRequest)
+		}
+		return
+	}
+
+	w.WriteHeader(http.StatusBadRequest)
+}
+
+func Run() {
+	shortUrls = map[string]string{}
+
+	router := http.NewServeMux()
+	router.HandleFunc(`/`, rootHandler)
+
+	err := http.ListenAndServe(":8080", router)
+
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ internal/app/app_test.go
@@ -0,0 +1,133 @@
+package app
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHandler_CreateShortUrl(t *testing.T) {
+	type want struct {
+		code        int
+		method      string
+		contentType string
+	}
+	type data struct {
+		url         string
+		contentType string
+	}
+	tests := []struct {
+		name string
+		data data
+		want want
+	}{
+		{
+			name: "positive test #1",
+			data: data{
+				url:         "https://ya.ru/",
+				contentType: "text/plain",
+			},
+			want: want{
+				code:        201,
+				contentType: "text/plain",
+			},
+		},
+		{
+			name: "positive test #2",
+			data: data{
+				url:         "",
+				contentType: "text/plain",
+			},
+			want: want{
+				code:        201,
+				contentType: "text/plain",
+			},
+		},
+		{
+			name: "negative test #1",
+			data: data{
+				url:         "",
+				contentType: "application/json",
+			},
+			want: want{
+				code:        400,
+				contentType: "text/plain",
+			},
+		},
+	}
+	shortUrls = map[string]string{}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			request := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(test.data.url))
+			request.Header.Set("Content-Type", test.data.contentType)
+
+			w := httptest.NewRecorder()
+			rootHandler(w, request)
+
+			res := w.Result()
+			assert.Equal(t, test.want.code, res.StatusCode)
+
+			if res.StatusCode != 400 {
+				defer res.Body.Close()
+				resBody, err := io.ReadAll(res.Body)
+
+				require.NoError(t, err)
+				assert.Contains(t, res.Header.Get("Content-Type"), test.want.contentType)
+				assert.NotEqual(t, "", string(resBody))
+			}
+		})
+	}
+}
+
+func TestHandler_RedirectShortUrl(t *testing.T) {
+	type want struct {
+		code int
+	}
+	tests := []struct {
+		name string
+		url  string
+		want want
+	}{
+		{
+			name: "positive test #1",
+			url:  "/abcdefgh",
+			want: want{
+				code: 307,
+			},
+		},
+		{
+			name: "positive test #2",
+			url:  "/dcbahgfe",
+			want: want{
+				code: 307,
+			},
+		},
+		{
+			name: "negative test #1",
+			url:  "/ffffffff",
+			want: want{
+				code: 400,
+			},
+		},
+	}
+	shortUrls = map[string]string{
+		"abcdefgh": "https://ya.ru",
+		"dcbahgfe": "https://mail.ru",
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			request := httptest.NewRequest(http.MethodGet, test.url, nil)
+
+			w := httptest.NewRecorder()
+			rootHandler(w, request)
+
+			res := w.Result()
+			assert.Equal(t, test.want.code, res.StatusCode)
+		})
+	}
+}
