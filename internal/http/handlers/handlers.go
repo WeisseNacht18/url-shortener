d11c937d1a0b8550b4e131ccb662ece99d56e552
WhiteNight18 <white18night@yandex.ru>
2024-10-03 22:54:43 +0300

    try to solve problem with malformed MIME header line #9

--- internal/http/handlers/handlers.go
+++ internal/http/handlers/handlers.go
@@ -103,6 +103,7 @@ func GzipHandle(next http.HandlerFunc) http.HandlerFunc {
 		if strings.Contains(r.Header.Get("Content-Encoding"), "gzip") &&
 			(strings.Contains(r.Header.Get("Content-Type"), "application/json") ||
 				strings.Contains(r.Header.Get("Content-Type"), "text/html")) {
+
 			gz, err := gzip.NewReader(r.Body)
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
